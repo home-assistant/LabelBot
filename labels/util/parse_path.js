0eb0b03ef5c5bdcb41ca2d5d4b8953bf01386cfc
Paulus Schoutsen <paulus@paulusschoutsen.nl>
2018-01-12 00:02:35 -0800

    Add parse_path helper

---
+++ labels/util/parse_path.js
@@ -0,0 +1,129 @@
+// Components that have multiple files that are not integrations
+var noEntityComponent = [
+  'alexa',
+  'cloud',
+  'config',
+  'google_assistant',
+  'http',
+  'mqtt',
+  'recorder',
+  'zwave',
+  'scene',
+];
+
+var coreComponents = [
+  'alarm_control_panel',
+  'alexa',
+  'api',
+  'automation',
+  'binary_sensor',
+  'camera',
+  'climate',
+  'cloud',
+  'configurator',
+  'conversation',
+  'counter',
+  'cover',
+  'device_tracker',
+  'fan',
+  'frontend',
+  'google_assistant',
+  'group',
+  'hassio',
+  'history',
+  'http',
+  'input_boolean',
+  'input_datetime',
+  'input_number',
+  'input_select',
+  'input_text',
+  'introduction',
+  'light',
+  'lock',
+  'logger',
+  'map',
+  'media_player',
+  'mqtt',
+  'notify',
+  'panel_custom',
+  'panel_iframe',
+  'persistent_notification',
+  'recorder',
+  'scene',
+  'script',
+  'sensor',
+  'shell_command',
+  'shopping_list',
+  'sun',
+  'switch',
+  'timer',
+  'updater',
+  'vacuum',
+  'weather',
+  'weblink',
+  'websocket_api',
+  'zone',
+];
+
+var coreComponentPlatforms = [
+  'automation',
+];
+
+module.exports = function(path) {
+  const parts = path.split('/');
+
+  // filter out all but the src
+  if (parts.shift() !== 'homeassistant') return null;
+
+  const result = {
+    core: false,
+    type: null,
+    component: null,
+    platform: null,
+  };
+
+  const subfolder = parts.shift();
+
+  if (subfolder !== 'components') {
+    result.core = true;
+
+    if (subfolder.endsWith('.py')) {
+      result.type = 'core';
+    } else {
+      result.type = subfolder;
+    }
+    return result;
+  }
+
+  if (parts.length == 1) {
+    if (parts[0] === 'services.yaml') {
+      result.type = 'services';
+    } else {
+      result.type = 'component';
+      result.component = parts[0].replace('.py', '');
+    }
+    return result;
+  }
+
+  result.component = parts.shift();
+
+  if (parts[0] === 'services.yaml') {
+    result.type = 'services'
+  } else if (parts[0] == '__init__.py' ||
+      noEntityComponent.includes(result.component)) {
+    result.type = 'component';
+  } else {
+    result.type = 'platform';
+    result.platform = parts[0].replace('.py', '');
+  }
+
+  if (result.type === 'component' &&
+      coreComponents.includes(result.component)) {
+    result.core = true;
+  } else if (result.type === 'platform' &&
+      coreComponentPlatforms.includes(result.component)) {
+    result.core = true;
+  }
+
+  return result;
+}
---
+++ test/util/parse_path.spec.js
@@ -0,0 +1,97 @@
+var assert = require('assert');
+
+var parsePath = require('../../labels/util/parse_path');
+
+describe('parsePath', () => {
+  it('detects core', () => {
+    result = parsePath('homeassistant/core.py');
+    assert.equal(result.core, true);
+    assert.equal(result.type, 'core');
+  });
+
+  it('detects helpers', () => {
+    result = parsePath('homeassistant/helpers/entity.py');
+    assert.equal(result.core, true);
+    assert.equal(result.type, 'helpers');
+  });
+
+  it('detects util', () => {
+    result = parsePath('homeassistant/util/__init__.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, null);
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'util');
+  });
+
+  it('detects scripts', () => {
+    result = parsePath('homeassistant/scripts/benchmark.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, null);
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'scripts');
+  });
+
+  it('detect component from component that is a single file', () => {
+    result = parsePath('homeassistant/components/keyboard.py');
+    assert.equal(result.core, false);
+    assert.equal(result.component, 'keyboard');
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'component');
+  });
+
+  it('detect component from component that is dir with init file', () => {
+    result = parsePath('homeassistant/components/group/__init__.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, 'group');
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'component');
+  });
+
+  it('detect component when not entity component', () => {
+    result = parsePath('homeassistant/components/mqtt/server.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, 'mqtt');
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'component');
+  });
+
+  it('detect core entity component', () => {
+    result = parsePath('homeassistant/components/light/__init__.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, 'light');
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'component');
+  });
+
+  it('detect core automation platform', () => {
+    result = parsePath('homeassistant/components/automation/state.py');
+    assert.equal(result.core, true);
+    assert.equal(result.component, 'automation');
+    assert.equal(result.platform, 'state');
+    assert.equal(result.type, 'platform');
+  });
+
+  it('detect platform', () => {
+    result = parsePath('homeassistant/components/light/hue.py');
+    assert.equal(result.core, false);
+    assert.equal(result.component, 'light');
+    assert.equal(result.platform, 'hue');
+    assert.equal(result.type, 'platform');
+  });
+
+  it('mark core services', () => {
+    result = parsePath('homeassistant/components/services.yaml');
+    assert.equal(result.core, false);
+    assert.equal(result.component, null);
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'services');
+  });
+
+  it('mark component services', () => {
+    result = parsePath('homeassistant/components/light/services.yaml');
+    assert.equal(result.core, false);
+    assert.equal(result.component, 'light');
+    assert.equal(result.platform, null);
+    assert.equal(result.type, 'services');
+  });
+});
